ba4dc683195bd3e75936122abb49d800cf59fb37
alejandro suaza <alejandrosuaza.1022@gmail.com>
2020-05-28 06:05:12 -0400

    cambio de configuración de conexión

--- services/postgres.js
+++ services/postgres.js
@@ -5,13 +5,13 @@ const path = require('path')
 require('dotenv').config({
     path: path.resolve(__dirname, '../.env')
 })
-class s_pg1 {
+class s_pg {
     constructor() {
-        const port = process.env.DB_PORT || 4000
-        const host = process.env.DB_HOST || 4000
-        const user = process.env.DB_USER || 4000
-        const database = process.env.DB_NAME || 4000
-        const password = process.env.DB_PASS || 4000
+        const port = "5432"
+        const host = "ec2-34-230-149-169.compute-1.amazonaws.com"
+        const user = "crirojimoyfovc"
+        const database = "d8rnvkq5i6q51n"
+        const password = "b0033587c42348346d0b8be24e51716202da2f930f23db2d638994515382b51b"
         this.pool = new Pool({
             user: user,
             host: host,
@@ -24,26 +24,5 @@ class s_pg1 {
         return await this.pool.query(sql, params);
     }
 
-
-}
-
-const {
-    Client
-} = require('pg');
-
-class s_pg {
-    constructor() {
-        this.client = new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: {
-                rejectUnauthorized: false
-            }
-        });
-    }
-
-    async eje_sql(sql, params) {
-        this.client.connect()
-        return await this.client.query(sql, params);
-    }
 }
 module.exports = s_pg;
