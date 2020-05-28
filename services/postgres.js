7745bd39e9204bd2bcfa1d9e5500981e13d59e68
alejandro suaza <alejandrosuaza.1022@gmail.com>
2020-05-28 05:27:44 -0400

    listo para desplegar

--- services/postgres.js
+++ services/postgres.js
@@ -3,12 +3,17 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 class s_pg {
     constructor() {
+        const port = process.env.DB_PORT || 4000
+        const host = process.env.DB_HOST || 4000
+        const user = process.env.DB_USER || 4000
+        const database = process.env.DB_NAME || 4000
+        const password = process.env.DB_PASS || 4000
         this.pool = new Pool({
-            user: process.env.DB_USER,
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASS,
-            port: process.env.DB_PORT
+            user: user,
+            host: host,
+            database: database,
+            password: password,
+            port: port
         });
     }
     async eje_sql(sql, params) {
