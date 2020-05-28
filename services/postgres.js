1348af3175387cc251c5a5ccff321763c4b85f65
alejandro suaza <alejandrosuaza.1022@gmail.com>
2020-05-28 05:41:11 -0400

    intento de conexión a base de datos

--- services/postgres.js
+++ services/postgres.js
@@ -1,7 +1,11 @@
-const { Pool } = require("pg");
+const {
+    Pool
+} = require("pg");
 const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
-class s_pg {
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env')
+})
+class s_pg1 {
     constructor() {
         const port = process.env.DB_PORT || 4000
         const host = process.env.DB_HOST || 4000
@@ -21,5 +25,25 @@ class s_pg {
     }
 
 
+}
+
+const {
+    Client
+} = require('pg');
+
+class s_pg {
+    constructor() {
+        this.client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+    }
+
+    async eje_sql(sql, params) {
+        this.client.connect()
+        return await this.client.query(sql, params);
+    }
 }
 module.exports = s_pg;
